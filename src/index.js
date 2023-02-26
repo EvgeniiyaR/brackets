0b52d9ec95549f60a2923be140b862e4fa45f05f
EvgeniiyaR <tulpansik@mail.ru>
2023-02-27 01:09:51 +0300

    init: complete bracket

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let list = [];
+  if (str.length % 2 != 0) {
+    return false;
+  }
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (str[i] === '|' && bracketsConfig[j][0] === '|' && list[list.length - 1] === '|') {
+        list.pop();
+      } else if (str[i] === '7' && bracketsConfig[j][0] === '7' && list[list.length - 1] === '7') {
+        list.pop();
+      } else if (str[i] === '8' && bracketsConfig[j][0] === '8' && list[list.length - 1] === '8') {
+        list.pop();
+      } else if (str[i] === bracketsConfig[j][0]) {
+        list.push(bracketsConfig[j][0]);
+      } else if (str[i] === bracketsConfig[j][1] && bracketsConfig[j][0] === list[list.length - 1] ) {
+        list.pop();
+      }
+    }
+  }
+  if (list.length != 0) {
+    return false;
+  }
+  return true;
 }
